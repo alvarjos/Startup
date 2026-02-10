f2d728699c5ff7fb441ae6c7afeb11cc55606b63
Angel <jangelalvarado02@gmail.com>
2026-02-09 22:03:09 -0700

    added reactDemo practice

--- reactDemo/index.jsx
+++ reactDemo/index.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './styles.css';
+
+function Page({ color }) {
+  return (
+    <div className="page" style={{ backgroundColor: color }}>
+      <h1>{color}</h1>
+    </div>
+  );
+}
 
 function App() {
-  const [bgColor, setBgColor] = React.useState('white');
-
-  const handleClick = () => {
-    setBgColor(
-        bgColor === 'white' ? 'green' : 
-        bgColor === 'green' ? 'red' :
-        'white'
-    );
-  };
-
   return (
-    <div onClick={handleClick} style={{ backgroundColor: bgColor, height: '100vh', font: 'bold 20vh Times New Roman', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <div> Hello React </div>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <nav>
+          <NavLink to="/">Red</NavLink>
+          <NavLink to="/orange">Orange</NavLink>
+          <NavLink to="/purple">Purple</NavLink>
+        </nav>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Page color="red" />} exact />
+            <Route path="/orange" element={<Page color="orange" />} />
+            <Route path="/purple" element={<Page color="purple" />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
