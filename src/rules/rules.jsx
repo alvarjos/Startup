a688855201b5f6a22cccdab8a65434bdf03fb615
Angel <jangelalvarado02@gmail.com>
2026-02-10 18:19:12 -0700

    added templates for jsx files for startup

---
+++ src/rules/rules.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Rules() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Rules displayed here</div>
+    </main>
+  );
+}
