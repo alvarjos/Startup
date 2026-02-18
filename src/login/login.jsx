c606c7c664cd401be0c214069db8540a7bd001b5
Angel <jangelalvarado02@gmail.com>
2026-02-17 21:54:50 -0700

    added login.jsx page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Login displayed here</div>
+    <main className="background-image1">
+      <nav>
+          <ul className="nav-bar">
+              <NavLink to="/home.html">Home Page</NavLink>
+              <NavLink to="/login.html">Login</NavLink>
+              <NavLink to="/table.html">Play Game</NavLink>
+              <NavLink to="/scores.html">High Scores</NavLink>
+              <NavLink to="/rules.html">Game Rules</NavLink>
+          </ul>
+      </nav>
+      Login Form:
+      <form className="login-form" action="/submit_login" method="post">
+          <label className="login-form-label" htmlFor="username">Username:</label>
+          <input className="login-form-label" type="text" id="username" name="username" required /><br /><br />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required /><br /><br />
+          <input type="submit" value="Login" />
+      </form>    
     </main>
   );
 }
