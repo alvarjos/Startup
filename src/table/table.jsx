694dadc2ffe9ea8feadc7080c233f25f18d97998
Angel <jangelalvarado02@gmail.com>
2026-03-02 22:38:49 -0700

    removed chat log

--- src/table/table.jsx
+++ src/table/table.jsx
@@ -173,47 +173,44 @@ export function Table({ user, onPlayerWin }) {
             <button className="table-button" onClick={handleStand} disabled={gameState === 'dealerTurn' || gameState === 'roundOver' || playerHand.length === 0}>Stand</button>
           </div>
         </section>
-            {/* <!-- Console Log
-            <section id="consoleLog">
-                Game messages and updates would be displayed here -->
+
         
-        
-        <!-- Chat messages will appear here --> */}
-          <section id="chatLog">
-            <div className="chat-header">Chat Log</div>
+    {/* Chat messages will appear here  
+        //   <section id="chatLog">
+        //     <div className="chat-header">Chat Log</div>
 
-            <div className="chat-messages">
-              <div className="chat-row">
-                <div className="chat-user">User 001</div>
-                <div className="chat-messages">Message will go right here</div>
-              </div>
+        //     <div className="chat-messages">
+        //       <div className="chat-row">
+        //         <div className="chat-user">User 001</div>
+        //         <div className="chat-messages">Message will go right here</div>
+        //       </div>
 
-              <div className="chat-row">
-                  <div className="chat-user">User 002</div>
-                  <div className="chat-messages">Another message will go right here</div>
-              </div>
+        //       <div className="chat-row">
+        //           <div className="chat-user">User 002</div>
+        //           <div className="chat-messages">Another message will go right here</div>
+        //       </div>
 
-              <div className="chat-row">
-                <div className="chat-user">User 003</div>
-                <div className="chat-messages">Another message will go right here</div>
-              </div>
+        //       <div className="chat-row">
+        //         <div className="chat-user">User 003</div>
+        //         <div className="chat-messages">Another message will go right here</div>
+        //       </div>
 
-              <div className="chat-row">
-                <div className="chat-user">User 004</div>
-                <div className="chat-messages">Another message will go right here</div>
-              </div>
+        //       <div className="chat-row">
+        //         <div className="chat-user">User 004</div>
+        //         <div className="chat-messages">Another message will go right here</div>
+        //       </div>
 
-              <div className="chat-row">
-                <div className="chat-user">User 005</div>
-                <div className="chat-messages">Another message will go right here</div>
-              </div>
+        //       <div className="chat-row">
+        //         <div className="chat-user">User 005</div>
+        //         <div className="chat-messages">Another message will go right here</div>
+        //       </div>
 
-              <div className="chat-row">
-                <div className="chat-user">User 006</div>
-                <div className="chat-messages">Another message will go right here</div>
-              </div>
-            </div>    
-          </section>
-        </main>
+        //       <div className="chat-row">
+        //         <div className="chat-user">User 006</div>
+        //         <div className="chat-messages">Another message will go right here</div>
+        //       </div>
+        //     </div>    
+        //   </section> */} 
+    </main> 
   );
 }
