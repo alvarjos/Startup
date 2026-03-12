5ab2bd91e9450010b2f0d357b836043cace1a949
Angel <jangelalvarado02@gmail.com>
2026-03-11 20:39:44 -0600

    setup simon-like api's for user and scores

--- service/index.js
+++ service/index.js
@@ -1,3 +1,112 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
+let users = [];
+let scores = [];
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the front-end static content hosting
 app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('username', req.body.username)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.username, req.body.password);
+    setAuthCookie(res, user.token);
+    res.send({ username: user.username });
+  }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('username', req.body.username);
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        user.token = uuid.v4();
+        setAuthCookie(res, user.token);
+        res.send({ username: user.username });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+  });
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+      next();
+    } else {
+      res.status(401).send({ msg: 'Unauthorized' });
+    }
+  };
+
+// GetScores
+apiRouter.get('/scores', verifyAuth, (_req, res) => {
+  console.log("In Scores");
+  res.send(scores);
+}); 
+
+// Test
+var testdata = {test:"testdata"};
+apiRouter.get('/test', verifyAuth, (_req, res) => {
+  console.log("In Test");
+  res.send(testdata);
+});
+
+// SubmitScore
+apiRouter.post('/score', verifyAuth, (req, res) => {
+  scores = updateScores(req.body);
+  res.send(scores);
+});
+
+// updateScores considers a new score for inclusion in the high scores.
+function updateScores(newScore) {
+    let found = false;
+    for (const [i, prevScore] of scores.entries()) {
+      if (newScore.score > prevScore.score) {
+        scores.splice(i, 0, newScore);
+        found = true;
+        break;
+      }
+    }
+  
+    if (!found) {
+      scores.push(newScore);
+    }
+  
+    if (scores.length > 10) {
+      scores.length = 10;
+    }
+  
+    return scores;
+  }
