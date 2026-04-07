87f3e68cd3a911d9d86f774571feb3c6dab99d83
Angel <jangelalvarado02@gmail.com>
2026-04-07 15:48:20 -0600

    replaced chatClient file

--- src/chatClient.js
+++ src/chatClient.js
@@ -1,45 +1,48 @@
-const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
-const socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+// This file is obselete, replaced it with the websocket connection 
+// in the table.jsx. Keeping it for reference though.
 
-socket.onopen = (event) => {
-    appendMsg('system', 'websocket', 'connected');
-};
+// const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+// const socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
 
-// Display messages in the chat window 
-socket.onmessage = async (event) => {
-    const text = await event.data.text();
-    const chat = JSON.parse(text);
-    appendMsg('user', chat.name, chat.msg);
-};
+// socket.onopen = (event) => {
+//     appendMsg('system', 'websocket', 'connected');
+// };
 
-// If websocket is closed, disables the interface 
-socket.onclose = (event) => {
-    appendMsg('system', 'websocket', 'disconnected');
-    document.querySelector('#name-controls').disabled = true;
-    document.querySelector('#chat-controls').disabled = true;
-};
+// // Display messages in the chat window 
+// socket.onmessage = async (event) => {
+//     const text = await event.data.text();
+//     const chat = JSON.parse(text);
+//     appendMsg('user', chat.name, chat.msg);
+// };
 
-// Send a message over websocket 
-function sendMessage() {
-    const msgEl = document.querySelector('#new-msg');
-    const msg = msgEl.value;
-    if (!!msg) {
-        appendMsg('me', 'me', msg);
-        const name = document.querySelector('#my-name').value;
-        socket.send(`{"name":"${name}", "msg":"${msg}"}`);
-        msgEl.value = '';
-    }
-}
+// // If websocket is closed, disables the interface 
+// socket.onclose = (event) => {
+//     appendMsg('system', 'websocket', 'disconnected');
+//     document.querySelector('#name-controls').disabled = true;
+//     document.querySelector('#chat-controls').disabled = true;
+// };
 
-function appendMsg(cls, from, msg) {
-    const chatText = document.querySelector('#chat-text');
-    chatText.innerHTML =
-    `<div><span class="${cls}">${from}</span>: ${msg}</div>` + chatText.innerHTML;
-}
+// // Send a message over websocket 
+// function sendMessage() {
+//     const msgEl = document.querySelector('#new-msg');
+//     const msg = msgEl.value;
+//     if (!!msg) {
+//         appendMsg('me', 'me', msg);
+//         const name = document.querySelector('#my-name').value;
+//         socket.send(`{"name":"${name}", "msg":"${msg}"}`);
+//         msgEl.value = '';
+//     }
+// }
 
-const input = document.querySelector('#new-msg');
-input.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        sendMessage();
-    }
-});
+// function appendMsg(cls, from, msg) {
+//     const chatText = document.querySelector('#chat-text');
+//     chatText.innerHTML =
+//     `<div><span class="${cls}">${from}</span>: ${msg}</div>` + chatText.innerHTML;
+// }
+
+// const input = document.querySelector('#new-msg');
+// input.addEventListener('keypress', (e) => {
+//     if (e.key === 'Enter') {
+//         sendMessage();
+//     }
+// });
